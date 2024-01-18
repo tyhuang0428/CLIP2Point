30673e73cc5fe17b3eb8676675f021459efc68dc
tudooh <1014090526@qq.com>
2024-01-18 20:36:21 +0800

    support zero-shot cls on ScanObjectNN

--- zeroshot.py
+++ zeroshot.py
@@ -4,8 +4,9 @@ import argparse
 from torch.utils.data import DataLoader
 import clip
 from tqdm import tqdm
+from pointnet2_ops import pointnet2_utils
 
-from datasets import ModelNet40Align, ModelNet40Ply
+from datasets import ModelNet10, ModelNet40Align, ModelNet40Ply, ScanObjectNN
 from render.selector import Selector
 from render.render import Renderer
 from utils import read_state_dict
@@ -14,14 +15,24 @@ clip_model, _ = clip.load('ViT-B/32', device='cpu')
 
 
 def inference(args):    
-    prompts = ['airplane', 'bathtub', 'bed', 'bench', 'bookshelf', 'bottle', 'bowl', 'car', 'chair', 'cone', 'cup', 'curtain', 'desk', 'door', 'dresser', 'flower pot', 'glass box', 'guitar', 'keyboard', 'lamp', 'laptop', 'mantel', 'monitor', 'night stand', 'person', 'piano', 'plant', 'radio', 'range hood', 'sink', 'sofa', 'stairs', 'stool', 'table', 'tent', 'toilet', 'tv stand', 'vase', 'wardrobe', 'xbox']
+    if args.dataset == 'ModelNet10':
+        dataset = ModelNet10()
+        prompts = ['bathtub', 'bed', 'chair', 'desk', 'dresser', 'monitor', 'night_stand', 'sofa', 'table', 'toilet']
+    elif args.dataset == 'ModelNet40':
+        dataset = ModelNet40Ply()
+        prompts = ['airplane', 'bathtub', 'bed', 'bench', 'bookshelf', 'bottle', 'bowl', 'car', 'chair', 'cone', 'cup', 'curtain', 'desk', 'door', 'dresser', 'flower pot', 'glass box', 'guitar', 'keyboard', 'lamp', 'laptop', 'mantel', 'monitor', 'night stand', 'person', 'piano', 'plant', 'radio', 'range hood', 'sink', 'sofa', 'stairs', 'stool', 'table', 'tent', 'toilet', 'tv stand', 'vase', 'wardrobe', 'xbox']
+    else:
+        dataset = ScanObjectNN()
+        prompts = ['bag', 'bin', 'box', 'cabinet', 'chair', 'desk', 'display', 'door', 'shelf', 'table', 'bed', 'pillow', 'sink', 'sofa', 'toilet']
+        
+
+    dataloader = DataLoader(dataset, batch_size=args.test_batch_size, num_workers=4, shuffle=True)
     prompts = ['image of a ' + prompts[i] for i in range(len(prompts))]
     prompts = clip.tokenize(prompts)
     prompts = clip_model.encode_text(prompts)
     prompts_feats = prompts / prompts.norm(dim=-1, keepdim=True)
-    dataset = ModelNet40Ply()
-    dataloader = DataLoader(dataset, batch_size=args.test_batch_size, num_workers=4, shuffle=True)
-
+    
+    # =================================== INIT MODEL ===========================================================
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model = deepcopy(clip_model.visual).to(device)
     if args.ckpt is not None:
@@ -29,15 +40,21 @@ def inference(args):
     selector = Selector(args.views, 0).to(device)
     render = Renderer(points_per_pixel=1, points_radius=0.02).to(device)
     prompt_feats = prompts_feats.to(device)
-
+    # ==================================== TESTING LOOP =====================================================
     model.eval()
     with torch.no_grad():
         correct_num = 0
         total = 0
         for (points, label) in tqdm(dataloader):
             points = points.to(device)
+            if args.dataset == 'ScanObjectNN':
+                fps_idx = pointnet2_utils.furthest_point_sample(points, 1024)
+                points = pointnet2_utils.gather_operation(points.transpose(1, 2).contiguous(), fps_idx).transpose(1, 2).contiguous()
             c_views_azim, c_views_elev, c_views_dist = selector(points)
-            images = render(points, c_views_azim, c_views_elev, c_views_dist, args.views, rot=True)
+            if args.dataset == 'ScanObjectNN':
+                images = render(points, c_views_azim, c_views_elev, c_views_dist, args.views, rot=False)
+            else:
+                images = render(points, c_views_azim, c_views_elev, c_views_dist, args.views, rot=True)
             b, n, c, h, w = images.shape
             images = images.reshape(-1, c, h, w)
             image_feats = model(images)
@@ -55,6 +72,7 @@ def inference(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Zero-shot Point Cloud Classification')
+    parser.add_argument('--dataset', type=str, choices=['ModelNet10', 'ModelNet40', 'ScanObjectNN'])
     parser.add_argument('--views', type=int, default=6)
     parser.add_argument('--ckpt', type=str, default=None)
     parser.add_argument('--test_batch_size', type=int, default=32, metavar='batch_size',
